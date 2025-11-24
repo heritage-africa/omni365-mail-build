c8882694a7f39d52cc0b79dd6f1639a0fb2a5fcd
fapathe <100762010+fapathe@users.noreply.github.com>
2025-11-24 11:42:41 +0000

    Update mailFilter.ts --

--- src/models/mailFilter.ts
+++ src/models/mailFilter.ts
@@ -1,6 +1,6 @@
 /**
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later on
  */
 import { randomId } from '../util/randomId'
 
